33f9d5d5ce09667b8c29896d302978b31866b35e
Zhipeng Cao <32233966+szczp02@users.noreply.github.com>
2021-03-01 15:08:45 -0600

    Removed Redundant

--- FDMLS/RFShim/@FD/mtimes.m
+++ FDMLS/RFShim/@FD/mtimes.m
@@ -12,13 +12,3 @@ Dx = x([1,1:(end-1)],:,:) - x([2:end,end],:,:);
 Dy = x(:,[1,1:(end-1)],:) - x(:,[2:end,end],:);
 
 y = cat(4,Dx,Dy);
-
-function y = adjDx(x)
-y = x([1,1:end-1],:,:) - x;
-y(1,:,:) = -x(1,:,:);
-y(end,:,:) = x(end-1,:,:);
-
-function y = adjDy(x)
-y = x(:,[1,1:end-1],:) - x;
-y(:,1,:) = -x(:,1,:);
-y(:,end,:) = x(:,end-1,:);
